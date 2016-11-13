019fad8d9640a8d15cdf4f1abbb12817d31364c2
Matt Harrison <iwasinnamuknow@genestate.com>
2016-11-13 07:12:21 +0000

    More docs and refactoring

--- talking-bin.rb
+++ talking-bin.rb
@@ -8,18 +8,20 @@ require 'uri'
 
 GT_URL="http://translate.google.com/translate_tts?ie=UTF-8&client=tw-ob&q=$1&tl=En-us"
 
-lines = YAML.load(File.read("lines.yml"))
-generator = RealRand::RandomOrg.new
-rand_data = generator.randnum(1000, 0, lines.count - 1)
-rand_data_pos = 0
+@lines = YAML.load(File.read("lines.yml"))
+@generator = RealRand::RandomOrg.new
 
 def refresh_rand_data
-	rand_data = generator.randnum(1000, 0, lines.count - 1)
-	rand_data_pos = 0
+  @rand_data = @generator.randnum(1000, 0, @lines.count - 1)
+  @rand_data_pos = 0
+end
+
+def get_wav_path(key)
+	return File.join(File.expand_path(File.dirname(__FILE__)), "cache", "#{key}.wav")
 end
 
 def cached?(key)
-	return File.file?("cache/#{key}.wav")
+	return File.file?(get_wav_path(key))
 end
 
 def fill_cache(text)
@@ -27,23 +29,27 @@ def fill_cache(text)
 	data = Net::HTTP.get(URI(URI.escape(url)))
 	key = Digest::SHA256.hexdigest(text)
 
-	File.open("cache/#{key}.wav", 'w') do |f|
+	File.open(get_wav_path(key), 'w') do |f|
 		f.write(data)
 	end
 end
 
+@rand_data = nil
+@rand_data_pos = nil
+refresh_rand_data
+
 loop do
-	puts lines[rand_data[rand_data_pos]]
-	key = Digest::SHA256.hexdigest(lines[rand_data[rand_data_pos]])
+	saying = @lines[@rand_data[@rand_data_pos]]
+	key = Digest::SHA256.hexdigest(saying)
 
 	if !cached?(key)
-		fill_cache(lines[rand_data[rand_data_pos]])
+		fill_cache(saying)
 	end
 
-	system("/usr/bin/mplayer -ao alsa -really-quiet -noconsolecontrols cache/#{key}.wav")
+	system("/usr/bin/mplayer -ao alsa -really-quiet -noconsolecontrols #{get_wav_path(key)}")
 
-	rand_data_pos = rand_data_pos + 1
-	if rand_data_pos > rand_data.size
+	@rand_data_pos = @rand_data_pos + 1
+	if @rand_data_pos > @rand_data.size
 		refresh_rand_data
 	end
 
