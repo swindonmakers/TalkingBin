36b80eee656d70cafd0b6a292a56935282183d78
Matt Harrison <iwasinnamuknow@genestate.com>
2016-11-15 20:53:56 +0000

    added very basic token based auth

--- app.rb
+++ app.rb
@@ -6,23 +6,36 @@ require 'config'
 module TalkingBin
   class App < WEBrick::HTTPServlet::AbstractServlet
 	  def do_GET request, response
-		  begin
-   			@cache = Cache.instance	
 
-        saying = PhraseManager.instance.get_random
- 			  filename = @cache.get(saying)
- 
-	 			pid = spawn("/usr/bin/mplayer -ao alsa -really-quiet -noconsolecontrols #{filename}")
-				Process.detach(pid)
- 			
-				response.status = 200
-	 			response['Content-Type'] = 'text/plain'
-	 			response.body = saying
- 			rescue Exception => e
-				response.status = 500
-	 			response['Content-Type'] = 'text/plain'
-				response.body = "Exception: #{e.message}"
+			if good_token?(request.query["token"])
+  		  begin
+     			@cache = Cache.instance	
+  
+          saying = PhraseManager.instance.get_random
+   			  filename = @cache.get(saying)
+   
+  	 			pid = spawn("/usr/bin/mplayer -ao alsa -really-quiet -noconsolecontrols #{filename}")
+  				Process.detach(pid)
+   			
+  				response.status = 200
+  	 			response['Content-Type'] = 'text/plain'
+  	 			response.body = saying
+   			rescue Exception => e
+  				response.status = 500
+  	 			response['Content-Type'] = 'text/plain'
+  				response.body = "Exception: #{e.message}"
+  			end
+			else
+  				response.status = 401
+  	 			response['Content-Type'] = 'text/plain'
+  	 			response.body = "Access denied"
 			end
+		end
+
+		private
+		
+		def good_token?(token)
+			return (token == Config.instance.get("auth_token"))
 		end
 	end
 end
