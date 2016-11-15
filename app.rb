e743c0158d4841b26d639e1552a1222b0d5f8032
Matt Harrison <iwasinnamuknow@genestate.com>
2016-11-15 18:17:52 +0000

    catch all those exceptions, or just some

--- app.rb
+++ app.rb
@@ -6,18 +6,23 @@ require 'config'
 module TalkingBin
   class App < WEBrick::HTTPServlet::AbstractServlet
 	  def do_GET request, response
- 			@cache = Cache.instance
+		  begin
+   			@cache = Cache.instance	
+
+        saying = PhraseManager.instance.get_random
+ 			  filename = @cache.get(saying)
+ 
+	 			pid = spawn("/usr/bin/mplayer -ao alsa -really-quiet -noconsolecontrols #{filename}")
+				Process.detach(pid)
  			
- 			saying = PhraseManager.instance.get_random
- 
- 			response.status = 200
- 			response['Content-Type'] = 'text/plain'
- 			response.body = saying
- 
- 			filename = @cache.get(saying)
- 
- 			pid = spawn("/usr/bin/mplayer -ao alsa -really-quiet -noconsolecontrols #{filename}")
-			Process.detach(pid)
+				response.status = 200
+	 			response['Content-Type'] = 'text/plain'
+	 			response.body = saying
+ 			rescue Exception => e
+				response.status = 500
+	 			response['Content-Type'] = 'text/plain'
+				response.body = "Exception: #{e.message}"
+			end
 		end
 	end
 end
